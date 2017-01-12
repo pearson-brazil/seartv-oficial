351b73bf9d0ab907af05c230a940cc7e81aeafb7
Willian <w.policiano@gmail.com>
2017-01-11 23:35:04 -0200

    Estilos e cores alterados para Dark

---
+++ app/src/main/java/br/com/pearson/seartv/model/CrewSerieMember.java
@@ -0,0 +1,50 @@
+package br.com.seartv.model;
+
+public class CrewSerieMember {
+
+    private String department;
+    private long id;
+    private String name;
+    private String job;
+    private String profile_path;
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getProfile_path() {
+        return profile_path;
+    }
+
+    public void setProfile_path(String profile_path) {
+        this.profile_path = profile_path;
+    }
+}
