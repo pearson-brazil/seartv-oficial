683a817ca435620bc9c303c991a534c10a862188
Willian <w.policiano@gmail.com>
2017-01-15 18:08:41 -0200

    [U5A1] Cria Splash

---
+++ app/src/main/java/br/com/pearson/seartv/ui/SplashActivity.java
@@ -0,0 +1,31 @@
+package br.com.pearson.seartv.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import br.com.seartv.R;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+        timer.start();
+    }
+}
