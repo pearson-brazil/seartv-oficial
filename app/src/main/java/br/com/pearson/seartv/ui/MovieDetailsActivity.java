54a5a7980628929740964d3bfbffc2b21f4990aa
Willian <w.policiano@gmail.com>
2017-01-15 19:32:00 -0200

    [U5A2] Controller para detalhes do filme criado

---
+++ app/src/main/java/br/com/pearson/seartv/ui/MovieDetailsActivity.java
@@ -0,0 +1,52 @@
+package br.com.pearson.seartv.ui;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import br.com.seartv.R;
+
+/**
+ * Created by Willian on 15/01/2017.
+ */
+
+public class MovieDetailsActivity extends BaseActivity {
+    private Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+
+        this.context = this;
+
+        setHomeButton();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        fillInformation();
+    }
+
+    private void fillInformation() {
+
+    }
+
+}
