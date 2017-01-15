14ac38f46c95c4e0420edc17026e4b2d6187b256
Willian <w.policiano@gmail.com>
2017-01-15 12:18:44 -0200

    [U4A1] Organiza pacotes e move HomeActivity para pacote ui

--- app/src/main/java/br/com/pearson/seartv/ui/HomeActivity.java
+++ app/src/main/java/br/com/pearson/seartv/ui/HomeActivity.java
@@ -1,7 +1,9 @@
-package br.com.pearson.seartv;
+package br.com.pearson.seartv.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.pearson.seartv.R;
 
 public class HomeActivity extends AppCompatActivity {
 
