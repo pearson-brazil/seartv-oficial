92844ee3d3ce1f0776a02fe3625ae2015a5f1b88
Willian <w.policiano@gmail.com>
2017-01-15 14:03:56 -0200

    [U4A1] Corrige pacote da BaseActivity e configura menu overflow na HomeActivity

--- app/src/main/java/br/com/pearson/seartv/ui/HomeActivity.java
+++ app/src/main/java/br/com/pearson/seartv/ui/HomeActivity.java
@@ -1,15 +1,37 @@
 package br.com.pearson.seartv.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import br.com.pearson.seartv.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setHomeButton();
+        setToolbarTitle(R.string.movies);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            case R.id.about:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
